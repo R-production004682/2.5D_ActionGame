49e32b8a22b979d6e0458bf4d1991234986056c5
Kr004682zero@icloud.com <Kr004682zero@icloud.com>
2025-07-19 21:43:43 +0900

    Gimmick周りのコードのリファクタリング

---
+++ 2.5D_ActionGame/Assets/Script/Gimmick/GimmickController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickController : MonoBehaviour
+{
+    [SerializeField] private GimmickData gimmickData;
+
+    private GimmickContext context;
+
+    private IGimmickLogic gimmickLogic;
+    private IGimmickEffect gimmickEffect;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+
+        context = new GimmickContext(
+            gimmickData,
+            rb,
+            this
+         );
+
+        gimmickLogic = GetComponent<IGimmickLogic>();
+        if(gimmickLogic != null)
+        {
+            gimmickLogic.Initialize(context);
+        }
+
+        gimmickEffect = GetComponent<IGimmickEffect>();
+        if(gimmickEffect != null)
+        {
+            gimmickEffect.Initialize(context);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(gimmickLogic != null)
+        {
+            gimmickLogic.Execute();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gimmickEffect.ApplyEffect(other);
+    }
+}
